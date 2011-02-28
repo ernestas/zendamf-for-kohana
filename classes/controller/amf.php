d5402212abb0de11d8fe405599f29ad8712864ed
Gabriel <gabriel@gartz.com.br>
2011-02-27 23:21:07 -0300

    Fixed ZF load only if ZF ins't already loaded

--- classes/controller/amf.php
+++ classes/controller/amf.php
@@ -14,18 +14,15 @@ class Controller_Amf extends Controller {
         
 		// Load config file
 		$this->config = Kohana::config('zendamf-for-kohana');
-
+		if( ! class_exists( Zend_Amf_Server ) )
+			$this->_loadZendAmfServer();
+		
 		// Instance Zend Amf Server object
 		$this->server = new Zend_Amf_Server();
 		$this->server->setProduction($this->config->production);
 		
 		if($this->config->amfbrowser)
 			$this->_enableZamfBrowser();
-	}
-    
-	public function action_index()
-	{
-		// Can override to add custom setClass(), this is for tests porpouses
 	}
     
 	public function after()
@@ -48,6 +45,23 @@ class Controller_Amf extends Controller {
 		$this->server->setClass( "ZendAmfServiceBrowser" );
         // Set a reference to the Zend_Amf_Server object so ZendAmfServiceBrowser class can retrive method information.
 		ZendAmfServiceBrowser::$ZEND_AMF_SERVER = $this->server;
+	}
+	
+	private function _loadZendAmfServer()
+	{
+		/**
+		 * Load Zend classes.
+		 */
+		if ( $path = Kohana::find_file('vendor', 'Zend/Loader') )
+		{
+		    ini_set('include_path', ini_get('include_path').PATH_SEPARATOR.dirname(dirname($path)));
+
+		    require_once 'Zend/Loader/Autoloader.php';
+
+		    require_once 'Zend/Amf/Server.php';
+
+		    Zend_Loader_Autoloader::getInstance();
+		}
 	}
 	
 }
